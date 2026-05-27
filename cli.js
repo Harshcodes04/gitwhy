3ccf95a9a09f15743c7dff0ee9ab9429adf9eb42
Harshcodes04 <harshitjantwal0418@gmail.com>
2026-05-28 01:39:59 +0530

    updates cli parsing

--- cli.js
+++ cli.js
@@ -1,3 +1,17 @@
-#!/usr/bin/env node
+#!/usr/bin/env node  //shebang to make the file executable
 
-console.log("GitWhy is running");
+const { execSync } = require("child_process");
+
+const file = process.argv[2];
+
+const blameOutput = execSync(`git blame ${file}`).toString();
+
+const firstLine = blameOutput.split("\n")[0];
+
+const commitHash = firstLine.split(" ")[0];
+
+const commitMessage = execSync(
+  `git show -s --format=%s ${commitHash}`,
+).toString();
+
+console.log("Commit:", commitMessage);
---
+++ index.js
@@ -0,0 +1,5 @@
+const hehe = () => {
+  console.log("Hello");
+};
+
+hehe();
