b2d893ceb3538316f8837bd118c2394dd83e9b1c
Harshcodes04 <harshitjantwal0418@gmail.com>
2026-05-31 22:05:14 +0530

    Line specific analysis

--- cli.js
+++ cli.js
@@ -1,17 +1,35 @@
-#!/usr/bin/env node  //shebang to make the file executable
+#!/usr/bin/env node
 
 const { execSync } = require("child_process");
 
 const file = process.argv[2];
 
-const blameOutput = execSync(`git blame ${file}`).toString();
+const lineFlagIndex = process.argv.indexOf("--line");
+
+const lineNumber = lineFlagIndex !== -1 ? process.argv[lineFlagIndex + 1] : 1;
+
+const blameOutput = execSync(
+  `git blame -L ${lineNumber},${lineNumber} ${file}`,
+).toString();
 
 const firstLine = blameOutput.split("\n")[0];
 
 const commitHash = firstLine.split(" ")[0];
 
+if (commitHash === "00000000") {
+  console.log("File has uncommitted changes.");
+  process.exit();
+}
+
 const commitMessage = execSync(
   `git show -s --format=%s ${commitHash}`,
 ).toString();
 
-console.log("Commit:", commitMessage);
+console.log(`
+WHY THIS CODE EXISTS
+
+Line: ${lineNumber}
+
+Commit:
+${commitMessage}
+`);
